6db0103d8c26e4832979cb5cf75b4b8ad6b65e20
samettasogluu <stasoglu178@gmail.com>
2024-11-14 15:04:47 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <BattlePage></BattlePage>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import BattlePage from './BattlePage.vue';
+  //
+</script>
---
+++ src/BattlePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1 class="bg-purple text-center py-5 BüyüjkBaşlık"> VERSUS</h1>
+    <h1 style="color: purple;">VERSUS</h1>
+
+    <section name=" Oyuncu HTML'i " class="mt-16 mx-14">
+        <p class="Başlık mb-2">Oyuncu</p>
+        <v-progress-linear height="40" color="green">
+        </v-progress-linear>
+    </section>
+
+
+    <section name=" Canavarın HTML'i " class="mt-16 mx-14">
+    <p class="Başlık mb-2">Canavar</p>
+    <v-progress-linear height="40" color="green">
+    </v-progress-linear>
+</section>
+
+    <section name="  Tuşlar "></section>
+    
+</template>
+
+<script setup></script>
+
+<style scoped>
+.BüyükBaşlık {
+    font-size: 60px;
+    font-weight: 400;
+    font-family: Impact, Haettenschweiler, 'sans-serif';
+
+    .Başlık {
+    font-size: 60px;
+    font-weight: 400;
+    font-family: Impact, Haettenschweiler, 'sans-serif';
+}
+}
+</style>
